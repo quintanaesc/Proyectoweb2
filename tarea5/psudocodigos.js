26ac38ed7984c238d6dc65112c7a6883d1f90a61
robertoquintana <2im3.quintana.esc.azul@gmail.com>
2022-11-29 00:43:55 -0600

    Se termino la implementacion de la tarea 5 y se hicieron correcciones en las funciones de la misma

--- tarea5/psudocodigos.js
+++ tarea5/psudocodigos.js
@@ -5,18 +5,18 @@ function imprime(){
 function suma(){
     let x=parseFloat(prompt("Ingresa un numero flotante: "));
     let y=parseFloat(prompt("Ingresa otro numero flotante: "));
-    alert("El resultado de sumarlos es"+(x+y));
+    alert("El resultado de sumarlos es "+(x+y));
 }
 
 function logicaInverso(){
     let x=Boolean(prompt("Ingresa True o False (1 = True/ 0 = False): "));
-    alert("El inverso del valor ingresado es"+(!x));
+    alert("El inverso del valor ingresado es "+(!x));
 }
 function logicaBinaria(){
-    let x=Boolean(input("Ingresa True o False (1 = True/ 0 = False): "));
-    let y=Boolean(input("Ingresa True o False (1 = True/ 0 = False): "));
+    let x=Boolean(prompt("Ingresa True o False (1 = True/ 0 = False): "));
+    let y=Boolean(prompt("Ingresa True o False (1 = True/ 0 = False): "));
     let z=prompt("Ingresa una operacion logica binaria (and / or / xor): ");
-    let toRes="El resultado de"+x+" "+z+" "+y+"es "
+    let toRes="El resultado de "+x+" "+z+" "+y+" es "
     alert(toRes+operaLog(x,y,z));
 }
 
@@ -27,11 +27,11 @@ function operaLog(x, y,  z){
     }
 
 function secNum(){
-    let cuenta
-    for(let i=0; i++;i<11) {
-        cuenta += String(i)+"!\n";
+    let cuenta="!Contemos!\n";
+    for(let i=1; i<11;i++) {
+        cuenta += i+"!\n";
     }
-    alert("!Contemos!\n"+cuenta);
+    alert(cuenta);
 }
 
 function adivinaInador(){//adivina el numero del 1 al 10
